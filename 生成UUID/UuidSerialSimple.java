285c5d2e5eb6f18c7643b795e842f3e262737439
BlissYang91 <2812420513@qq.com>
2019-06-18 15:04:01 +0800

    UUID

---
+++ 生成UUID/UuidSerialSimple.java
@@ -0,0 +1,14 @@
+package cn.kudesoft.daka.utils;
+
+import java.util.UUID;
+
+/**
+ * @autor YangTianFu
+ * @Date 2019/4/29  10:22
+ * @Description
+ */
+public class UuidSerialSimple {
+    public static String getUniquePsuedoID(){
+        return UUID.randomUUID().toString().replaceAll("-","");
+    }
+}
